85e960defe90d249b8e5ef6cd6440dfecd5a6dc6
JotaC3 <joaocarloscostacarvalho@gmail.com>
2024-08-01 13:19:24 -0300

    a

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -40,11 +40,42 @@ export const Quiz = ({
     const  challenge = challenges[activeIndex];
     const options = challenge?.challengeOptions ?? []
 
+    const onNext  = () =>{
+        setActiveIndex((current) => current + 1)
+    }
+
     const onSelect = (id: number) =>{
         if(status !== "none") return;
 
         setSelectedOption(id);
     }
+
+    const onContinue= () =>{
+        if(!selectedOption) return;
+
+        if(status ==="wrong"){
+            setStatus("none");
+            setSelectedOption(undefined);
+            return
+        }
+        if(status ==="correct"){
+            onNext();
+            setStatus("none");
+            setSelectedOption(undefined);
+            return
+        }
+
+        const correctOption = options.find((option) =>option.correct);
+        if(!correctOption){
+            return
+        }
+
+        if(correctOption && correctOption.id === selectedOption){
+            console.log("Opção correta!");
+        } else{
+            console.error("Incorrect option ")
+        }
+    };
 
     const title = challenge.type === 'ASSIST' 
     ? 'Selecione a alternativa correta'
@@ -83,7 +114,7 @@ export const Quiz = ({
             <Footer 
                 disabled={!selectedOption}
                 status={status}
-                onCheck={()=>{}}
+                onCheck={onContinue}
             />
         </>
     );
