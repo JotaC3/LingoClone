2e892f4db2706e5b93df91f49ff5c397ddaeb09e
JotaC3 <joaocarloscostacarvalho@gmail.com>
2024-08-17 11:10:12 -0300

    a

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -15,16 +15,16 @@ type Props = {
 const marketingLayout = ({ children }: Props) => {
   return (
 
-      <div className="min-h-screen flex flex-col"> 
-        <Header></Header>
+    <div className="min-h-screen flex flex-col">
+      <Header></Header>
 
-        <main className="flex-1 flex flex-col items-center justify-center">
-          {children}
-        </main>
-          
+      <main className="flex-1 flex flex-col items-center justify-center">
+        {children}
+      </main>
+
       <Footer></Footer>
-    </div> 
-    )
+    </div>
+  )
 }
 
 export default marketingLayout
